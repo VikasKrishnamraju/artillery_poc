51ac31ae43fe97e667a5f2dcf9336af899173942
VikasKrishnamraju <vikas.krishnamraju@gmail.com>
2025-02-18 20:42:02 +0530

    Update flows.js

--- flows.js
+++ flows.js
@@ -10,10 +10,10 @@ const redis = new Redis({
   host: process.env.redisEndpoint,
 });
 
-async function createUser() {
+/*async function createUser() {
   await redis.set("foo", "bar");
   let x = await redis.get("foo");
-  console.log(x);
+  console.log(x);*/
 }
 
 async function getUser(context, events) {
