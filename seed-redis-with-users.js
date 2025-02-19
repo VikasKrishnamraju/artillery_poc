aaee8898cf14015e923dc34d1d2572b2d57d2a2d
VikasKrishnamraju <vikas.krishnamraju@gmail.com>
2025-02-19 18:36:08 +0530

    Create seed-redis-with-users.js

---
+++ seed-redis-with-users.js
@@ -0,0 +1,48 @@
+const Redis = require("ioredis");
+const falso = require('@ngneat/falso');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Configuration
+const USERS_COUNT = 100;
+const BATCH_SIZE = 5;
+
+// Initialize Redis
+const redis = new Redis({
+  port: Number(process.env.redisPort || 6379),
+  host: process.env.redisEndpoint,
+  connectTimeout: 10000,
+});
+
+// Generate a random username and password
+function generateUser() {
+  return {
+    username: falso.randUserName(),
+    password: falso.randPassword(),
+  };
+}
+
+async function storeUsersInRedis(users) {
+    const pipeline = redis.pipeline();
+    users.forEach(user => {
+      if (user) {
+        pipeline.lpush('users', JSON.stringify(user));
+      }
+    });
+    await pipeline.exec();
+  }
+
+// Main function to seed users
+async function seedUsers() {
+  for (let i = 0; i < USERS_COUNT; i += BATCH_SIZE) {
+    // Generate users
+    const users = Array.from({ length: BATCH_SIZE }, generateUser);
+    await storeUsersInRedis(users);
+    console.log(users);
+    console.log(`Batch ${i / BATCH_SIZE + 1} completed.`);
+  }
+  console.log('All users have been seeded and stored in Redis.');
+  process.exit(0);
+}
+
+seedUsers();
